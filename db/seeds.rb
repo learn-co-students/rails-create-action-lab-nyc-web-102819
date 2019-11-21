2e13798b6859041542cff5083527cd078f2a34e0
hdp2112 <hdp2112@columbia.edu>
2019-11-21 17:29:34 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+    Student.create(first_name: "Hector", last_name: "Polanco")
+end
